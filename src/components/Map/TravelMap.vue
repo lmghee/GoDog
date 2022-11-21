87f1fb8a04038d2d5e4dd71f175773034d5630ef
oliqoeiilo <oliqoeiilo@naver.com>
2022-11-22 08:57:43 +0900

    merge

--- src/components/Map/TravelMap.vue
+++ src/components/Map/TravelMap.vue
@@ -240,8 +240,12 @@ export default {
             position: new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude)), // 마커를 표시할 위치
             title: title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             image: markerImage, // 마커 이미지
+            // clickable: true,
           });
-
+          // kakao.maps.event.addListener(marker, "click", function () {
+          // 마커 위에 인포윈도우를 표시합니다
+          // infowindow.open(map, marker);
+          // });
           bounds.extend(new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude)));
 
           if (i === 0) {
